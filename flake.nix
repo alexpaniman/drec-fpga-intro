0dd4febf20468df72f0745655f3dbd3af548217a
alexpaniman <alexpaniman@gmail.com>
2024-04-20 21:48:39 +0300

    feature: nix integration

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        system = system;
+      };
+    in
+  {
+
+    devShells.x86_64-linux = {
+      default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.verilog
+          pkgs.gtkwave
+        ];
+      };
+    };
+
+  };
+}
