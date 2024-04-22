b375b877c6344ab18b528ee3fdd23be9fc89c868
alexpaniman <alexpaniman@gmail.com>
2024-04-23 00:57:26 +0300

    feature: solve 02_clock_div problem

--- problems/02_clock_div/testbench.v
+++ problems/02_clock_div/testbench.v
@@ -1,33 +1,38 @@
 `timescale 1 ns / 100 ps
 
 /*
-*   Introduction to FPGA and Verilog
-*
-*   Viktor Prutyanov, 2019
-*
-*   Testbench for problem set #02
-*/
+ *   Introduction to FPGA and Verilog
+ *
+ *   Viktor Prutyanov, 2019
+ *
+ *   Testbench for problem set #02
+ */
 
 module testbench();
 
-reg clk = 1'b0;
+   reg clk = 1'b0;
 
-always begin
-    #1 clk = ~clk;
-end
+   always begin
+      #1 clk = ~clk;
+   end
 
-/*
-*   Problem 2/3:
-*   Make an instance of your new module clk_div.
-*   Create approprite wires to test it.
-*/
-
-initial begin
-    $dumpvars;      /* Open for dump of signals */
-    /*
+   /*
     *   Problem 2/3:
-    *   Stop simulation after several cycles of your (divided) clock.
+    *   Make an instance of your new module clk_div.
+    *   Create approprite wires to test it.
     */
-end
+   wire clk_reduced;
+   clk_div #(.X(16)) clock_div(.clk(clk), .clk_reduced(clk_reduced));
+
+
+   initial begin
+      $dumpvars;      /* Open for dump of signals */
+      /*
+       *   Problem 2/3:
+       *   Stop simulation after several cycles of your (divided) clock.
+       */
+      $display("Test started...");
+      #1024 $finish;
+   end
 
 endmodule
