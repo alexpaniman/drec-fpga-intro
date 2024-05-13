998adf5ce24cde319a9b289243128990d75c9767
alexpaniman <alexpaniman@gmail.com>
2024-05-13 07:01:21 +0300

    feature: uart receiver

--- problems/other_uart/clock-divider.v
+++ problems/other_uart/clock-divider.v
@@ -1,13 +1,14 @@
 module clock_divider #(
   parameter NATIVE_CLK_FREQUENCY = 1,
-  parameter TARGET_CLK_FREQUENCY = 1
+  parameter TARGET_CLK_FREQUENCY = 1,
+  parameter DELAYED_START = 0
 ) (input wire clk, input wire reset, output wire target_clk);
    parameter REDUCTION_RATE = NATIVE_CLK_FREQUENCY / TARGET_CLK_FREQUENCY;
    
    reg [$clog2(REDUCTION_RATE):0]counter = 0;
 
    always @(posedge clk, posedge reset)
-       counter <= reset? 0 : counter + 1;
+       counter <= reset? DELAYED_START : counter + 1;
 
    assign target_clk = counter == REDUCTION_RATE;
 
--- problems/other_uart/testbench.v
+++ problems/other_uart/testbench.v
@@ -6,8 +6,12 @@ module testbench();
     reg [0:7]data = 8'b0;
     reg sending = 1'b0;
 
+    wire tx;
     uart_transmitter #(.NATIVE_CLK_FREQUENCY(100000), .BAUDRATE(9600))
-        uart_tx(.clk(clk), .sending(sending), .data(data));
+        uart_tx(.clk(clk), .sending(sending), .data(data), .tx(tx));
+
+    uart_receiver #(.NATIVE_CLK_FREQUENCY(100000), .BAUDRATE(9600))
+        uart_rx(.clk(clk), .rx(tx));
 
     initial begin
         $dumpvars;
